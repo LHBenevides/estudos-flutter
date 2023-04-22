70448936eecfb5128cc30e899dae5b259127e54c
Luiz Benevides <luiz_h.b@outlook.com>
2023-04-21 23:05:50 -0300

    create task_dao and class 'TaskDao'

---
+++ primeiro_projeto/lib/data/task_dao.dart
@@ -0,0 +1,44 @@
+import 'package:primeiro_projeto/components/task.dart';
+import 'package:primeiro_projeto/database.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TaskDao {
+  static const String tableSql = 'CREATE TABLE $_tablename('
+      '$_name TEXT, '
+      '$_difficulty INTEGER, '
+      '$_image TEXT,)';
+
+  static const String _tablename = 'tasktable';
+  static const String _name = 'name';
+  static const String _difficulty = '_difficulty';
+  static const String _image = 'image';
+
+  save(TaskCard tarefa) async {}
+
+  Future<List<TaskCard>> findAll() async {
+    print('acessando FindAll');
+    final Database database = await getDataBase();
+    final List<Map<String, dynamic>> result = await database.query(_tablename);
+    print(result);
+    return toList(result);
+  }
+
+  List<TaskCard> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('toList on');
+    final List<TaskCard> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final TaskCard tarefa = TaskCard(
+        linha[_name],
+        linha[_difficulty],
+        foto: linha[_image],
+      );
+      tarefas.add(tarefa);
+    }
+    print(tarefas);
+    return tarefas;
+  }
+
+  Future<List<TaskCard>> find(String momeDaTarefa) async {}
+
+  delete(String nomeDaTarefa) async {}
+}
