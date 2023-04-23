c45855bc5f6e109b1470f2eb3993ad9e1198076e
Luiz Benevides <luiz_h.b@outlook.com>
2023-04-23 19:40:29 -0300

    chore: update packages and instantiating new database

--- primeiro_projeto/lib/data/database.dart
+++ primeiro_projeto/lib/data/database.dart
@@ -2,13 +2,10 @@ import 'package:primeiro_projeto/data/task_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-Future<Database> getDataBase() async {
-  final String path = join(await getDatabasesPath(), 'task.db');
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'taskFinal.db');
   return openDatabase(path, onCreate: (db, version) {
     db.execute(TaskDao.tableSql);
-  }, version: 1);
+  }, version: 1,);
 }
-
-
-
 
