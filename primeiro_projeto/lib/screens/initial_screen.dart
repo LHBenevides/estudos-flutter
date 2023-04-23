be7e1cacf193e866279865dd339af1debb6f785d
Luiz Benevides <luiz_h.b@outlook.com>
2023-04-23 19:52:26 -0300

    feat: add widget 'FutureBuilder' in initial_screen

--- primeiro_projeto/lib/screens/initial_screen.dart
+++ primeiro_projeto/lib/screens/initial_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:primeiro_projeto/data/task_inherited.dart';
+import 'package:primeiro_projeto/components/task.dart';
+import 'package:primeiro_projeto/data/task_dao.dart';
+
+//import 'package:primeiro_projeto/data/task_inherited.dart';
 import 'package:primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
@@ -17,19 +20,91 @@ class _InitialScreenState extends State<InitialScreen> {
       appBar: AppBar(
         leading: Container(),
         title: const Text('Tarefas'),
+        actions: [IconButton(onPressed: (){setState(() {
+        });}, icon: const Icon(Icons.refresh))],
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.only(top: 10, bottom: 70),
-        children: TaskInherited.of(context).taskList,
+        child: FutureBuilder<List<TaskCard>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<TaskCard>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando...')
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando...')
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.active:
+                  return const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('Carregando...')
+                      ],
+                    ),
+                  );
+                  break;
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final TaskCard tarefa = items[index];
+                            return tarefa;
+                          });
+                    }
+                    return const Center(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            size: 128,
+                            color: Colors.grey,
+                          ),
+                          Text(
+                            'Não Há Nenhuma Tarefa',
+                            style: TextStyle(fontSize: 32, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                  return const Text('Erro ao Carregar Tarefas', style: TextStyle(color: Colors.grey));
+                  break;
+              }
+              return const Text('Error');
+            }),
       ),
+
+      // ListView(
+      //   padding: const EdgeInsets.only(top: 10, bottom: 70),
+      //   children: TaskInherited.of(context).taskList,
+      // ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (contextNew) => FormScreen(taskContext: context)
-              ),
-          );
+            context,
+            MaterialPageRoute(
+                builder: (contextNew) => FormScreen(taskContext: context)),
+          ).then((value) => setState((){}));
         },
         child: const Icon(Icons.add),
       ),
