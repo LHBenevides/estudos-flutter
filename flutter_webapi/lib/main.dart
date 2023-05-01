f44ae2c914c912aff97eb0b160a47084deea2bc5
Luiz Benevides <luiz_h.b@outlook.com>
2023-05-01 00:05:15 -0300

    feat: adicionando 'onGenerateRoute'

--- flutter_webapi/lib/main.dart
+++ flutter_webapi/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/add_journal_screen/add_journal_screen.dart';
 import 'screens/home_screen/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -19,14 +21,25 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black87,
           titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-          textTheme: GoogleFonts.bitterTextTheme(),
+        textTheme: GoogleFonts.bitterTextTheme(),
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == 'add-journal') {
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(builder: (context) {
+            return AddJournalScreen(journal: journal);
+          });
+        }
+        return null;
       },
     );
   }
