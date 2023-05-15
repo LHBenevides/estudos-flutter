d5fc1b8706f9f4ed461dd4be27266748a49f4326
Luiz Benevides <luiz_h.b@outlook.com>
2023-05-14 23:30:19 -0300

    feat: add register e edit em 'add_jornal_screen'

--- flutter_webapi/lib/screens/add_journal_screen/add_journal_screen.dart
+++ flutter_webapi/lib/screens/add_journal_screen/add_journal_screen.dart
@@ -1,15 +1,22 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_webapi_first_course/models/journal.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../helpers/logout.dart';
 import '../../helpers/weekday.dart';
+import '../commom/exception_dialog.dart';
 
 class AddJournalScreen extends StatelessWidget {
   final Journal journal;
-  AddJournalScreen({Key? key, required this.journal}) : super(key: key);
+  final bool isEditing;
+  AddJournalScreen({Key? key, required this.journal, required this.isEditing})
+      : super(key: key);
   final TextEditingController _contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    _contentController.text = journal.content;
     return Scaffold(
       appBar: AppBar(
         title: Text(WeekDay(journal.createdAt).toString()),
@@ -37,11 +44,48 @@ class AddJournalScreen extends StatelessWidget {
   }
 
   registerJournal(BuildContext context) {
-    String content = _contentController.text;
-    journal.content = content;
-    JournalService service = JournalService();
-    service.register(journal).then((value) {
-      Navigator.pop(context, value);
+    SharedPreferences.getInstance().then((prefs) {
+      String? token = prefs.getString("accessToken");
+      if (token != null) {
+        String content = _contentController.text;
+        journal.content = content;
+        JournalService service = JournalService();
+        if (isEditing) {
+          service.register(journal, token).then(
+            (value) {
+              Navigator.pop(context, value);
+            },
+          ).catchError(
+            (error) {
+              logout(context);
+            },
+            test: (error) => error is TokenNotValidException,
+          ).catchError(
+            (error) {
+              var innerError = error as HttpException;
+              showExceptionDialog(context, content: innerError.message);
+            },
+            test: (error) => error is HttpException,
+          );
+        } else {
+          service.edit(journal.id, journal, token).then(
+            (value) {
+              Navigator.pop(context, value);
+            },
+          ).catchError(
+            (error) {
+              logout(context);
+            },
+            test: (error) => error is TokenNotValidException,
+          ).catchError(
+            (error) {
+              var innerError = error as HttpException;
+              showExceptionDialog(context, content: innerError.message);
+            },
+            test: (error) => error is HttpException,
+          );
+        }
+      }
     });
   }
 }
